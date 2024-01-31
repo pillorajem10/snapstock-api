564eaaa83ebfcc99d6bbb8b1e96375a2d9ef3f66
unknown <pillorajem10@gmail.com>
2024-01-31 12:54:05 +0800

    modified server

--- server.js
+++ server.js
@@ -59,8 +59,11 @@ const io = socketIO(server, {
   secure: true
 });
 
-io.on("connect_error", (err) => {
-  console.log(`connect_error due to ${err.message}`);
+io.engine.on("connection_error", (err) => {
+  console.log(err.req);      // the request object
+  console.log(err.code);     // the error code, for example 1
+  console.log(err.message);  // the error message, for example "Session ID unknown"
+  console.log(err.context);  // some additional error context
 });
 
 io.on('connection', (socket) => {
