34ccd81acf7a983ec1c291826ca7d8cad03cbeb6
unknown <pillorajem10@gmail.com>
2024-03-03 22:19:57 +0800

    modified user routes

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,27 +1,31 @@
 // routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
-const {
-  list,
-  add,
-  addEmplooyeeUser,
-  requestNewPassword,
-  getById,
-  updateById,
-  deleteById,
-  verifyUser,
-  changePassword
-} = require('../controllers/user.js');
 
-// CRUD operations
-router.post('/', add);
-router.get('/', list);
-router.get('/:id', getById);
-router.post('/requestnewpassword', requestNewPassword);
-router.post('/changepassword/:token', changePassword);
-router.put('/:id', updateById);
-router.delete('/:id', deleteById);
-router.get('/verify/:token', verifyUser);
-router.post('/addemployeeuser', addEmplooyeeUser);
 
-module.exports = router;
+module.exports = (io) => {
+  const {
+    list,
+    add,
+    addEmplooyeeUser,
+    requestNewPassword,
+    getById,
+    updateById,
+    deleteById,
+    verifyUser,
+    changePassword
+  } = require('../controllers/user.js');
+
+  // CRUD operations
+  router.post('/', (req, res) => add(req, res, io));
+  router.get('/', list);
+  router.get('/:id', getById);
+  router.post('/requestnewpassword', requestNewPassword);
+  router.post('/changepassword/:token', changePassword);
+  router.put('/:id', updateById);
+  router.delete('/:id', deleteById);
+  router.get('/verify/:token', verifyUser);
+  router.post('/addemployeeuser', addEmplooyeeUser);
+
+  return router; // This line was missing in your original code
+};
